939f9dfa6fa40c6fbc734e1ac8aa7884bd7b65eb
robert-upwork <robyop.eyeco@gmail.com>
2021-09-15 12:24:56 +0700

    USDC & BUSD

--- frontend/src/views/dashboard/Exchange.js
+++ frontend/src/views/dashboard/Exchange.js
@@ -171,6 +171,40 @@ const Exchange = () => {
           err => setIsSubmit(false)
         )
       }
+      else if (transaction && transaction.from === 'USDC' && !isSubmit) {
+        setIsSubmit(true);
+        paymentService.postUSDTDetect({address: user.ETH_ADDRESS, contract: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'}).then(
+          result => {
+            setIsSubmit(false);
+            if (!result.error) {
+              if (result.balance >= Number(transaction.sendAmount) && runTimer) {
+                setRunTimer(false);
+                setTimeout(() => {
+                  saveTransactionForProgress()
+                }, 1500);
+              }
+            }
+          },
+          err => setIsSubmit(false)
+        )
+      }
+      else if (transaction && transaction.from === 'BUSD' && !isSubmit) {
+        setIsSubmit(true);
+        paymentService.postUSDTDetect({address: user.ETH_ADDRESS, contract: '0xe9e7cea3dedca5984780bafc599bd69add087d56'}).then(
+          result => {
+            setIsSubmit(false);
+            if (!result.error) {
+              if (result.balance >= Number(transaction.sendAmount) && runTimer) {
+                setRunTimer(false);
+                setTimeout(() => {
+                  saveTransactionForProgress()
+                }, 1500);
+              }
+            }
+          },
+          err => setIsSubmit(false)
+        )
+      }
       else if (transaction && !isSubmit) {
         setIsSubmit(true);
         paymentService.postOtherDetect({address: user.ETH_ADDRESS, contract: '0xdac17f958d2ee523a2206206994597c13d831ec7'}).then(
